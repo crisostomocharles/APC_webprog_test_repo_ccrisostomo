24a66b77dea27b1ba5c6c59efceea3d458e7f7f3
Charles Crisostomo <crisostomocharles032430@gmail.com>
2016-12-21 20:46:07 +0800

    wrong commit

--- exercise6/CodeIgniter/application/views/templates/header.php
+++
@@ -1,8 +0,0 @@
-<html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
- 
-                <h1>Simple CRUD</h1>
-                <p><a href="<?php echo site_url('form'); ?>">Home</a> | <a href="<?php echo site_url('form/create'); ?>">Add News</a></p>
